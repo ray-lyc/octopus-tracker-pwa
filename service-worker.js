363df28b82232575c4eed6a4b16d18d4758651f0
Ray <114709349+ray-lyc@users.noreply.github.com>
2026-06-12 08:10:39 +0100

    Fix PWA issues
    Change the version and ask client to update the cache for any change

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,43 @@
-const CACHE = "octopus-tracker-v1";
+const CACHE = "octopus-tracker-pwa-v1";
 
 const ASSETS = [
   "./",
   "./index.html",
-  "./manifest.json"
+  "./manifest.json",
+  "./favicon.ico",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
 ];
 
 self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE).then(cache => cache.addAll(ASSETS))
-  );
+  self.skipWaiting();
 });
 
 self.addEventListener("fetch", e => {
+  if (e.request.mode === "navigate") {
+
+    e.respondWith(
+      fetch(e.request).catch(() => caches.match("index.html"))
+    );
+
+    return;
+  }
+
   e.respondWith(
     caches.match(e.request).then(r => r || fetch(e.request))
   );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(keys =>
+            Promise.all(
+                keys
+                    .filter(key => key !== CACHE)
+                    .map(key => caches.delete(key))
+            )
+        ).then(() => clients.claim())
+    );
 });
 
 self.addEventListener('message', e => {
